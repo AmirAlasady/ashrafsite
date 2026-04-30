790b3048cbc7e2d3b0ead82c226b34d88632e9c2
AmirAlasady <amiralasady107@gmail.com>
2026-04-30 21:47:51 +0300

    BTS slider: slow it down (interval 2.4s -> 3.5s, slide 0.75s -> 1s)

--- static/js/animations.js
+++ static/js/animations.js
@@ -33,7 +33,7 @@
 
         var idx = 0;
         var total = slides.length;
-        var AUTO_INTERVAL_MS = 2400;
+        var AUTO_INTERVAL_MS = 3500;
         var autoTimer = null;
         var prefersReducedMotion = window.matchMedia &&
             window.matchMedia("(prefers-reduced-motion: reduce)").matches;
